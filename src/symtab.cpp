a9d1fe93454f79a45896995f289c810471a2cd5b
Abhay Mishra <mishraabhay9179913420@gmail.com>
2022-04-01 23:02:25 +0530

    initial implementation of symbol table

--- src/symtab.cpp
+++ src/symtab.cpp
@@ -2,21 +2,21 @@
 #include "type.h"
 using namespace std;
 
-int symadd(string symname, Type* symtype){	
+int symadd(string symname, Type* symtype){
 	//check new sym
 	if( symtab.find(symname) != symtab.end() ){
 		cout<< "ERROR: Repeat entry symbol table -"<<symname<<endl;
 		return -1;
 	}
-	
 	symtab[symname] = symtype;
 	return 0;
 }
 
-void print_symtab(){
-	cout<<"----SYMBOL TABLE----"<<endl;
-	cout<<"Scope_num Sym_name"<<endl<<endl;
+void print_symtab(ofstream& symbolTable){
+	symbolTable <<"----SYMBOL TABLE----"<<endl;
+	symbolTable <<"Scope_num Sym_name"<<endl<<endl;
 	for( auto i : symtab){
-		cout<<i.first<<endl;
+		symbolTable <<i.first<<endl;
 	}
+	symbolTable <<"----DONE----"<<endl;
 }
